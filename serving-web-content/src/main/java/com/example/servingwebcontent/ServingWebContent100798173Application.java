484160ed032d46886f4fd375b478c158236f91c6
Eisha Aqeel <eisha.aqeel@dcmail.ca>
2023-12-08 18:22:34 -0500

    initial commit - shows greeting

---
+++ serving-web-content/src/main/java/com/example/servingwebcontent/ServingWebContent100798173Application.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContent100798173Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContent100798173Application.class, args);
+	}
+
+}
